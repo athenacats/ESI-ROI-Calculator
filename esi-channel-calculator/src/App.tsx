d7d9055835106d5aa0c7830754d3321974798c18
athenacats <chenalonya@gmail.com>
2025-11-13 22:26:14 +0000

    Properly name file import

--- esi-channel-calculator/src/App.tsx
+++ esi-channel-calculator/src/App.tsx
@@ -1,9 +1,9 @@
-import EsiChannelPartnerRoiCalculator from "./components/ESIChannelPartnerCalculator";
+import ESIChannelPartnerCalculator from "./components/ESIChannelPartnerCalculator";
 
 function App() {
   return (
     <div className="min-h-screen bg-slate-50">
-      <EsiChannelPartnerRoiCalculator />
+      <ESIChannelPartnerCalculator />
     </div>
   );
 }
